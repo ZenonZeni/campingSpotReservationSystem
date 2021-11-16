29b2ea1ec637e3f5e8eaadc18e96082c974a9af5
Christopher Nguyen <ZenonZeni@hotmail.com>
2021-11-16 18:32:51 -0500

    Check if User Exists
    When loading main page check if user exists.

---
+++ JavaScript&PHP/checkUser.js
@@ -0,0 +1,47 @@
+//by:Christopher Nguyen | 216213332
+//Login page javascript using AJAX, Jquery and JSON to get credentials and check if valid or not.
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+	  alert("No previous user found")
+     //user = prompt("Please enter your name:","");
+     //if (user != "" && user != null) {
+       //setCookie("username", user, 30);
+     //}
+	 
+  }
+}
+
+function getCookie(pass) {
+  let username = pass + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(username) == 0) {
+      return c.substring(username.length, c.length);
+    }
+  }
+  return "";
+}
+
+function getPassword(pass) {
+  let password = pass + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(password) == 0) {
+      return c.substring(password.length, c.length);
+    }
+  }
+  return "";
+}
