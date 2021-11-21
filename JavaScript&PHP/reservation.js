62444eb2ad52509254c1a0d66a46dc01a5f91953
Christopher Nguyen <ZenonZeni@hotmail.com>
2021-11-21 01:54:41 -0500

    Fixing CSS issues with server
    This fixes the bottom part of booking after you select a campspot.

--- JavaScript&PHP/reservation.js
+++ JavaScript&PHP/reservation.js
@@ -2,28 +2,28 @@ $('#campsite').on('change',
   function() {
     var selectedCampsite = $(this).val();
 	if (selectedCampsite == 1) {
-		document.getElementById("camp-spot1").style.display  = "initial";
+		document.getElementById("camp-spot1").style.display  = "inline-block";
 		document.getElementById("camp-spot2").style.display  = "none";
 		document.getElementById("camp-spot3").style.display  = "none";
 		document.getElementById("camp-spot4").style.display  = "none";
 	}
 	else if (selectedCampsite == 2) {
 		document.getElementById("camp-spot1").style.display  = "none";
-		document.getElementById("camp-spot2").style.display  = "initial";//code
+		document.getElementById("camp-spot2").style.display  = "inline-block";//code
 		document.getElementById("camp-spot3").style.display  = "none";
 		document.getElementById("camp-spot4").style.display  = "none";
 	}
 	else if (selectedCampsite == 3){
 		document.getElementById("camp-spot1").style.display  = "none";
 		document.getElementById("camp-spot2").style.display  = "none";
-		document.getElementById("camp-spot3").style.display  = "initial";
+		document.getElementById("camp-spot3").style.display  = "inline-block";
 		document.getElementById("camp-spot4").style.display  = "none";
 	}
 	else if (selectedCampsite == 4) {
 		document.getElementById("camp-spot1").style.display  = "none";
 		document.getElementById("camp-spot2").style.display  = "none";
 		document.getElementById("camp-spot3").style.display  = "none";
-		document.getElementById("camp-spot4").style.display  = "initial";
+		document.getElementById("camp-spot4").style.display  = "inline-block";
 	}
 	else{
 		//null
@@ -35,12 +35,12 @@ $('#camp-spot1-choices').on('change',
   function() {
     var selectedCampspot = $(this).val();
 	if (selectedCampspot == 1) {
-		document.getElementById("camp-spot1-choices-1").style.display  = "initial";
+		document.getElementById("camp-spot1-choices-1").style.display  = "inline-block";
 		document.getElementById("camp-spot1-choices-2").style.display  = "none";
 	}
 	else if (selectedCampspot == 2) {
 		document.getElementById("camp-spot1-choices-1").style.display  = "none";
-		document.getElementById("camp-spot1-choices-2").style.display  = "initial";
+		document.getElementById("camp-spot1-choices-2").style.display  = "inline-block";
 	}
 	else if (selectedCampspot == 3){
 		//null
@@ -59,7 +59,7 @@ $('#camp-spot2-choices').on('change',
   function() {
     var selectedCampspot = $(this).val();
 	if (selectedCampspot == 1) {
-		document.getElementById("camp-spot2-choices-1").style.display  = "initial";
+		document.getElementById("camp-spot2-choices-1").style.display  = "inline-block";
 	}
 	else if (selectedCampspot == 2) {
 		//null
@@ -71,7 +71,7 @@ $('#camp-spot2-choices').on('change',
 		//null
 	}
 	else{
-		document.getElementById("camp-spot2-choices-1").style.display  = "none";
+		document.getElementById("camp-spot2-choices-1").style.display  = "inline-block";
 	}
   }
 );
@@ -80,7 +80,7 @@ $('#camp-spot3-choices').on('change',
   function() {
     var selectedCampspot = $(this).val();
 	if (selectedCampspot == 1) {
-		document.getElementById("camp-spot3-choices-1").style.display  = "initial";
+		document.getElementById("camp-spot3-choices-1").style.display  = "inline-block";
 	}
 	else if (selectedCampspot == 2) {
 		//null
@@ -92,7 +92,7 @@ $('#camp-spot3-choices').on('change',
 		//null
 	}
 	else{
-		document.getElementById("camp-spot3-choices-1").style.display  = "none";
+		document.getElementById("camp-spot3-choices-1").style.display  = "inline-block";
 	}
   }
 );
@@ -101,7 +101,7 @@ $('#camp-spot4-choices').on('change',
   function() {
     var selectedCampspot = $(this).val();
 	if (selectedCampspot == 1) {
-		document.getElementById("camp-spot4-choices-1").style.display  = "initial";
+		document.getElementById("camp-spot4-choices-1").style.display  = "inline-block";
 	}
 	else if (selectedCampspot == 2) {
 		//null
