d535c4f7179712f037dbe92662ae2e9acf6a83c8
Arslan <arslan_5000@outlook.com>
2021-11-27 11:27:27 -0500

    Updated profile page

--- JavaScript&PHP/accountsetting.js
+++ JavaScript&PHP/accountsetting.js
@@ -28,4 +28,31 @@ $("#reservation-button").on("click",function(){
     $("#notification-setting").css("display","none");
     $("#reservation-setting").css("display","inline-block");
 });
+
+$("#resevation-detial").on("click", function() {
+   $(".modal").css("display", "inline-block");
+});
+ $(".close"). click(function() {
+        $(".modal").css("display","none");
+
+});
+$(document).click(function (e) {
+    if ($(e.target).is(".modal")) {
+        $(".modal").css("display","none");
+    }
+
+});
+$(".review-button").on("click", function() {
+   $(".modal-review").css("display", "inline-block");
+});
+ $(".close-review"). click(function() {
+        $(".modal-review").css("display","none");
+
+});
+$(document).click(function (e) {
+    if ($(e.target).is(".modal-review")) {
+        $(".modal-review").css("display","none");
+    }
+
+});
 });
