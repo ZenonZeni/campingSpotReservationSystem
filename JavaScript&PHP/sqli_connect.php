02351c6182470e1a648989f04057377b8327c5ef
Christopher Nguyen <61134161+ZenonZeni@users.noreply.github.com>
2022-03-03 18:10:32 -0500

    Server Update
    Changing Server

--- JavaScript&PHP/sqli_connect.php
+++ JavaScript&PHP/sqli_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "remotemysql.com:3306";
-$username = "JSBoJ3Pjgp";
-$password = 'Ox6qU96aFL';
-$dbname = "JSBoJ3Pjgp";
+$servername = "sql5.freemysqlhosting.net:3306";
+$username = "sql5476730";
+$password = 'Jm6kWQFsyz';
+$dbname = "sql5476730";
 
 // Create connection to sql server
 $conn = new mysqli($servername, $username, $password, $dbname);
