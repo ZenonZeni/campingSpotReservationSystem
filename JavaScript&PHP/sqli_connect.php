7ad4c4f8496caff3bd8bb98cd3a173f30e23acae
Christopher Nguyen <61134161+ZenonZeni@users.noreply.github.com>
2022-03-12 03:31:56 -0500

    Server Update
    Changed to different server provider.

--- JavaScript&PHP/sqli_connect.php
+++ JavaScript&PHP/sqli_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql5.freemysqlhosting.net:3306";
-$username = "sql5476730";
-$password = 'Jm6kWQFsyz';
-$dbname = "sql5476730";
+$servername = "fdb33.awardspace.net:3306";
+$username = "4062578_main";
+$password = 'Password123!';
+$dbname = "4062578_main";
 
 // Create connection to sql server
 $conn = new mysqli($servername, $username, $password, $dbname);
